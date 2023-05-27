0f8e32a39f14466863f9bd8cd2c1050feaaf5c65
Nonoka Sasaki <94826346+ns1230@users.noreply.github.com>
2023-05-27 20:30:23 +0900

    Update record.h

--- record.h
+++ record.h
@@ -1,28 +1,28 @@
-/*****************************************************************
+//*****************************************************************
 //
 //  NAME:        Nonoka Sasaki
-//  //  //
-//  //  //  HOMEWORK:    project1
-//  //  //
-//  //  //  CLASS:       ICS 212
-//  //  //
-//  //  //  INSTRUCTOR:  Ravi Narayan
-//  //  //
-//  //  //  DATE:        March 12, 2020
-//  //  //
-//  //  //  FILE:        record.h
-//  //  //
-//  //  //  DESCRIPTION:
-//  //  //   This file is the record file(header) for project1.
-//  //  //   It contains basic informations needed for a record.
-//  //  ****************************************************************/
+//
+//  PROJECT:     Project1
+//  
+//  CLASS:       UHM ICS 212
+//
+//  DATE:        March 12, 2020;  refined May 27, 2023
+//  
+//  FILE:        database.c
+//  
+//  DESCRIPTION:
+//  database.h is a header file that defines the structure and function prototypes used in the database operations. 
+//  It contains the structure definition for the "record" data type, which represents a single record in the database. 
+//  The structure includes fields such as account number, name, and address.
+//  
+/****************************************************************/
 
 struct record
 {
     int                accountno;
     char               name[25];
     char               address[80];
-    struct record*     next;            //"struct record* next" and "struct record *next" means the same in c. '*' denotes that it is a pointer
+    struct record*     next;            
 };
 
 
