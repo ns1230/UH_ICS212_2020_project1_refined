6b358c1f181038cecd0337ce42a01f3aeedb36bf
Nonoka Sasaki <94826346+ns1230@users.noreply.github.com>
2023-05-27 20:33:28 +0900

    Update database.h

--- database.h
+++ database.h
@@ -1,23 +1,21 @@
-/*****************************************************************
+//*****************************************************************
 //
 //  NAME:        Nonoka Sasaki
-//  //  //
-//  //  //  HOMEWORK:    project1
-//  //  //
-//  //  //  CLASS:       ICS 212
-//  //  //
-//  //  //  INSTRUCTOR:  Ravi Narayan
-//  //  //
-//  //  //  DATE:        March 12, 2020
-//  //  //
-//  //  //  FILE:        database.h
-//  //  //
-//  //  //  DESCRIPTION:
-//  //  //   This file is the header file for database for project1.
-//  //  //   It contains addRecord, printRecord, printAllRecords, modifyrecord, and
-//  //  //   deleteRecord.
-//  //  //
-//  //  ****************************************************************/
+//
+//  PROJECT:     Project1
+//  
+//  CLASS:       UHM ICS 212
+//
+//  DATE:        March 12, 2020;  refined May 27, 2023
+//  
+//  FILE:        database.h
+//  
+//  DESCRIPTION:
+//  database.h is a header file that defines the structure and function prototypes used in the database operations. 
+//  It contains the structure definition for the "record" data type, which represents a single record in the database. 
+//  The structure includes fields such as account number, name, and address.
+//  
+/****************************************************************/
 
 #ifndef  _RECORD_H_
 #define _RECORD_H_
